b10c2e6967b66ff3d23407a04980c24e9f977608
Emanzahidd <emanzahidpriv@gmail.com>
2023-12-14 22:54:17 -0800

    connected with mongodb

---
+++ api.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect with MongoDB
+mongoose
+  .connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);no
+  });
+
+// Schema and Model
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+// POST (Create Product)
+app.post("/api/v1/product/new", async (req, res) => {
+  try {
+    const product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+    });
+
+    const savedProduct = await product.save();
+    res.json(savedProduct);
+  } catch (error) {
+    console.error("Error creating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// GET (Read Products)
+app.get("/api/v1/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// PUT (Update Product By ID)
+app.put("/api/v1/product/:id", async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const { name, description, price } = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { name, description, price },
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json(updatedProduct);
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// DELETE (Delete Product By ID)
+app.delete("/api/v1/product/delete/:id", async (req, res) => {
+  try {
+    const productId = req.params.id;
+
+    const deletedProduct = await Product.findByIdAndRemove(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json(deletedProduct);
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is working on http://localhost:${PORT}`);
+});
